6b92af29bd84473273eba05a81430c0a237152a2
Emmanuel <chiwuzoemmanuel@gmail.com>
2025-01-04 17:43:52 +0100

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      <main>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit sed
-        vitae blanditiis. Praesentium architecto quibusdam libero, deleniti
-        atque, tempora, facilis voluptatem quaerat id cum quisquam natus.
-        Temporibus, sed nisi! Impedit! Lorem ipsum dolor sit amet, consectetur
-        adipisicing elit. Fugit modi veniam quidem quaerat quas sint sapiente
-        suscipit consectetur unde, amet ipsum labore maiores iure, cupiditate
-        nihil eligendi voluptatum. Sunt, totam!
-      </main>
-    </div>
+    <main>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit sed
+      vitae blanditiis. Praesentium architecto quibusdam libero, deleniti atque,
+      tempora, facilis voluptatem quaerat id cum quisquam natus. Temporibus, sed
+      nisi! Impedit! Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Fugit modi veniam quidem quaerat quas sint sapiente suscipit consectetur
+      unde, amet ipsum labore maiores iure, cupiditate nihil eligendi
+      voluptatum. Sunt, totam!
+    </main>
   );
 }
