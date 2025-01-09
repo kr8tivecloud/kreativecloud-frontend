59b1a86eff0b47cba14bd0a637196d49aaaaf0dc
abraham <abrahamosazee3@gmail.com>
2025-01-09 13:07:45 +0100

    worked on entrepreneur page

---
+++ public/images/index.ts
@@ -0,0 +1,26 @@
+import enterprenuerHero from "./entreprenuer/hero.jpeg";
+import enterprenuerFashion from "./entreprenuer/enterprenuerFashion.jpeg";
+import enterprenuerProject1 from "./entreprenuer/enterprenuer1.jpeg";
+import enterprenuerProject2 from "./entreprenuer/enterprenuer2.jpeg";
+import enterprenuerProject3 from "./entreprenuer/enterprenuer3.jpeg";
+import linkedin from "./entreprenuer/svgs/linkedIn.svg";
+import twitter from "./entreprenuer/svgs/twitter.svg";
+import facebook from "./entreprenuer/svgs/facebook.svg";
+import instagram from "./entreprenuer/svgs/instagram.svg";
+
+const entrepreneurPage = {
+  enterprenuerHero,
+  enterprenuerFashion,
+  enterprenuerProject1,
+  enterprenuerProject2,
+  enterprenuerProject3,
+};
+
+const socialLinks = {
+  facebook,
+  twitter,
+  instagram,
+  linkedin,
+};
+
+export default { entrepreneurPage, socialLinks };
---
+++ src/app/(home)/entrepreneur/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import Hero from "./components/hero";
+
+const EntrepreneurPage = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      {/* HERO SECTION */}
+      <Hero />
+
+      {/* PROJECT SECTION */}
+      <div></div>
+    </div>
+  );
+};
+
+export default EntrepreneurPage;
