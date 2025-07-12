bde81af66c4634640f9b60193f888a958c25b779
Emmanuel <chiwuzoemmanuel@gmail.com>
2025-07-12 11:12:18 +0100

    changes

--- src/app/(home)/portfolio/page.tsx
+++ src/app/(home)/portfolio/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { KreativeCloud } from "../../../../public/images";
 import PortfolioItems from "./components/portfolio-items";
 import { motion } from "framer-motion";
@@ -9,36 +9,52 @@ import { motion } from "framer-motion";
 export default function PortfolioPage() {
   const [pageLoaded, setPageLoaded] = React.useState(false);
   const [animationEnded, setAnimationEnded] = React.useState(false);
+  const [startAnimation, setStartAnimation] = useState(false);
 
-  React.useEffect(() => {
-    let timer: NodeJS.Timeout;
-    if (typeof window !== "undefined") {
-      timer = setTimeout(() => {
-        setPageLoaded(true);
-      }, 2000);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const onLoad = () => {
+      setPageLoaded(true);
+    };
+
+    // Check if document has already loaded
+    if (document.readyState === "complete") {
+      onLoad();
+    } else {
+      window.addEventListener("load", onLoad);
     }
+
+    // Cleanup
     return () => {
-      if (timer) clearTimeout(timer);
+      window.removeEventListener("load", onLoad);
     };
   }, []);
 
+  useEffect(() => {
+    let timer: number;
+    if (pageLoaded) {
+      timer = window.setTimeout(() => setStartAnimation(true), 300);
+    }
+    return () => window.clearTimeout(timer);
+  }, [pageLoaded]);
+
   return (
-    <div className="mt-28">
+    <div className="mt-32">
       {/* ENTRANCE OVERLAY */}
       {!animationEnded && (
         <motion.div
-          data-loaded={pageLoaded}
+          data-start={startAnimation}
           initial={{
             opacity: 100,
           }}
           animate={
-            pageLoaded && {
+            startAnimation && {
               opacity: 0,
             }
           }
           transition={{
-            delay: 2,
-            duration: 0.5,
+            delay: 1.5,
+            duration: 0.3,
           }}
           onAnimationComplete={() => setAnimationEnded(true)}
           className="absolute inset-0 bg-[#FE922A] z-50"
@@ -47,10 +63,10 @@ export default function PortfolioPage() {
       {/* END ENTRANCE OVERLAY */}
       {/* TITLE */}
       <motion.div
-        transition={{ duration: 0.8, ease: "easeOut" }}
-        data-loaded={pageLoaded}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        data-start={startAnimation}
         layout
-        className="px-4 sm:container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 data-[loaded=true]:static data-[loaded=true]:top-0 data-[loaded=true]:left-0 data-[loaded=true]:-translate-x-0 data-[loaded=true]:-translate-y-0"
+        className="px-4 sm:container absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 data-[start=true]:relative data-[start=true]:top-0 data-[start=true]:left-0 data-[start=true]:-translate-x-0 data-[start=true]:-translate-y-0"
       >
         <div className="text-2xl lg:text-3xl text-center max-w-[801px] mx-auto pt-10 pb-12">
           At{" "}
